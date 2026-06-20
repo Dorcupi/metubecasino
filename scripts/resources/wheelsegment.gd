f4a683f9bef741441e2826aec22c1f771abba06d
Dorcupi <98676609+Dorcupi@users.noreply.github.com>
2026-06-19 22:19:39 -0600

    Basic design & frameworks
    Got a basic design done and frameworks that I'll need as I head into full production

--- scripts/resources/wheelsegment.gd
+++ scripts/resources/wheelsegment.gd
@@ -4,12 +4,49 @@ class_name WheelSegment
 
 @export var texture: Texture2D
 ## The texture of the wheel segment.
-@export var is_benefitial: bool
-## Whether the result is benefitial to a players score or not.
 @export var static_add_modifier: float
 ## A modifier to add (or subtract with negatives) onto the final return
 @export var static_multiply_modifier: float
 ## A modifier to multiply (or divide with fractions) onto the final return
 
-func add_modifier() -> ScoreUpdate:
-	return
+func add_modifier(old_score: float) -> float:
+	var new_score: float = old_score
+	new_score += static_add_modifier
+	new_score = new_score * static_multiply_modifier
+	return new_score
+
+func generate_message(old_score: float, new_score: float) -> String:
+	var add_string: String = ""
+	var multiply_string: String = ""
+	var final_string: String = ""
+	if static_add_modifier != 0:
+		add_string = "%+f" % static_add_modifier
+	if static_multiply_modifier != 0:
+		if -1 < static_multiply_modifier and static_multiply_modifier < 1:
+			var division_number: float = 1 / static_multiply_modifier
+			multiply_string = "/ %f" % division_number
+		else:
+			multiply_string = "* %f" % static_multiply_modifier
+	if add_string == "" and multiply_string == "":
+		final_string = "+0"
+	else:
+		if add_string != "" and multiply_string != "":
+			final_string = add_string + " " + multiply_string
+		elif add_string != "" and multiply_string == "":
+			final_string = add_string
+		else:
+			final_string = multiply_string
+	return final_string
+
+func generate_update(old_score: float) -> ScoreUpdate:
+	var updated_score: ScoreUpdate = ScoreUpdate.new()
+	var new_score: float = add_modifier(old_score)
+	var message: String = generate_message(old_score, new_score)
+	var is_benefitial: bool = false
+	var benefitial_determiner: float = new_score - old_score
+	if benefitial_determiner >= 0:
+		is_benefitial = true
+	updated_score.new_score = new_score
+	updated_score.message = message
+	updated_score.is_benefitial = is_benefitial
+	return updated_score
